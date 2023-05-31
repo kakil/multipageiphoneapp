e3759793f054a4ace2d34104d23abb9cde519b59
Kitwana Akil <kakil@me.com>
2023-05-31 11:59:57 -0400

    Initial Commit

---
+++ MultiPage iPhone/MultiPage_iPhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiPage_iPhoneApp.swift
+//  MultiPage iPhone
+//
+//  Created by Kitwana Akil on 5/31/23.
+//
+
+import SwiftUI
+
+@main
+struct MultiPage_iPhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
